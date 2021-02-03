53734fb00055998ade8429e35319cbd5d7eb0b4b
Sholanki <63105386+Sholanki@users.noreply.github.com>
2021-02-03 11:12:26 +0530

    Update .eleventy.js

--- .eleventy.js
+++ .eleventy.js
@@ -17,7 +17,7 @@ module.exports = function (eleventyConfig) {
   eleventyConfig.addPassthroughCopy("css");
  return {
     dir: {
-      input: "src",
+      input: "_site",
       output: "public",
     },
   };
